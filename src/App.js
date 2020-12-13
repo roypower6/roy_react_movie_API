0f0ef256688c4df8e5f3d24236c217c19a4c8e0a
이승기 <iseung-gi@iseung-giui-MacBookAir.local>
2020-12-13 17:39:46 +0900

    first react moviepage API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
+/* eslint-disable */
+import React, { useState } from 'react';
 import './App.css';
+import './Movie.css';
+import axios from 'axios';
+import Movie from "./Movie";
 
-function App() {
+
+class App extends React.Component{
+
+  state={
+    isLoading: true
+  }
+
+  getMovies= async() =>{
+    const {
+      data:{
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
+        this.setState({ movies, isLoading: false});
+  }
+  componentDidMount(){
+    this.getMovies()
+  }
+  render(){
+    const {isLoading, movies}= this.state;
   return (
-    <div className="App">
-      
-    </div>
-  );
+  <section className="container">
+    {isLoading ? (
+    <div className="loader">
+      <span className="loader__text">Loading...</span>
+      </div>
+      ) : (
+          <div className="movies">
+            {movies.map(movie => (
+          <Movie  
+            key={movie.id}
+            id={movie.id} 
+            year ={movie.year}
+            title ={movie.title}
+            summary ={movie.summary}
+            poster ={movie.medium_cover_image}
+            genres = {movie.genres}
+          />
+          ))}
+          </div>
+      )}
+      </section>
+      );
+  }
 }
 
 export default App;
